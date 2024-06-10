d1ba5ea36534c921699dfd7c873d022c8e2b1ab5
Marco Castellani <marcocastellanictt@gmail.com>
2024-06-10 20:22:41 -0300

    node

---
+++ N-ACCONTS/index.js
@@ -0,0 +1,10 @@
+// modulos externos 
+const inquirer = require('inquirer')
+const chalk = require('chalk')
+
+// Modulos Internos
+const fs = require('fs')
+
+console.log("Iniciamos o Accounts");
+
+
--- NodeJS/3 - Core Modules/10_roteamento/index.js
+++ NodeJS/3 - Core Modules/10_roteamento/index.js
@@ -17,7 +17,12 @@ const server = http.createServer( (req, res) => {
                 return res.end()
             })
         }else{
-
+            //404
+            fs.readFile('404.html', function(err,data) {
+                res.writeHead(404, {'Content-Type': 'text/html'})
+                res.write(data)
+                return res.end()
+            })
         }
     }
 })
